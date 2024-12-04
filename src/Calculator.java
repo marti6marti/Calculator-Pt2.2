3ef47f22770cd94e5da40ffe7f8baf60780204f4
marti6marti <41652609t@iespoblenou.org>
2024-12-04 18:27:41 +0100

    branch i modifacat

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,9 +6,7 @@ public class Calculator {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
-        return a * b;
-    }
+
 
     public int divide(int a, int b){
         if (a==0 ||b==0){
