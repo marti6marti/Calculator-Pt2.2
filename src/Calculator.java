91ed12918e17d0ec22cb3a7d76b50692cddb024a
marti6marti <41652609t@iespoblenou.org>
2024-12-05 11:47:18 +0100

    faltava un;

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,14 +5,18 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-
-
-
+    public int multyply(int a,int b){
+        return a*b;
+    }
     public int divide(int a, int b){
         if (a==0 ||b==0){
             return 0;
         }
         return a/b;
+    }
+
+    public int modulus(int a,int b){
+        return a%b;
     }
     public double sqrt(double a) {
         return Math.sqrt(a);
@@ -22,6 +26,8 @@ public class Calculator {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
         System.out.println("Subtraction: " + calculator.subtract(10, 5));
+        System.out.println("Modulus" + calculator.modulus(10,5));
+        System.out.println("Multiply" + calculator.multyply(10,5));
         System.out.println("Divide: " + calculator.divide(10, 5));
         if (calculator.divide(0,5)==0){
             System.out.println("Error");
